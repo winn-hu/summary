7cd3e29aa305ee9a30a47f50415d6c4e259f0699
Winn <1147565914@qq.com>
2019-07-01 16:30:18 +0800

    json

---
+++ comm/src/main/java/utils/JsonUtil.java
@@ -0,0 +1,44 @@
+package utils;
+
+import com.google.gson.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Map;
+
+public class JsonUtil {
+
+    private static Gson gson;
+
+    public static Map json2map(String json) {
+        return getGson().fromJson(json,Map.class);
+    }
+
+    public static String map2json(Map<String, Object> map){
+        return getGson().toJson(map);
+    }
+
+    public static List json2list(String json){
+        return getGson().fromJson(json,List.class);
+    }
+
+    public static String list2json(List list){
+        return getGson().toJson(list);
+    }
+
+    private static Gson getGson(){
+        if(gson == null){
+            gson = new GsonBuilder()
+                    .setDateFormat(Constant.TIME_FORMAT)
+                    .registerTypeAdapter(LocalDateTime.class,
+                            (JsonSerializer<LocalDateTime>) (src, typeOfSrc, context) ->
+                                    new JsonPrimitive(src.format(DateTimeFormatter.ofPattern(Constant.TIME_FORMAT))))
+                    .registerTypeAdapter(LocalDate.class, (JsonSerializer<LocalDate>) (src, typeOfSrc, context) ->
+                            new JsonPrimitive(src.format(DateTimeFormatter.ofPattern(Constant.DATE_FORMAT))))
+                    .create();
+        }
+         return gson;
+    }
+}
