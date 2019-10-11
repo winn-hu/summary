cb38baf815ef252af24c6884683c795e733d81a5
Winn <1147565914@qq.com>
2019-10-11 14:01:13 +0800

    'vue'

---
+++ vue/vue-project/vue/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <!-- 跳转指向的页面会通过路由在这里展示 -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
