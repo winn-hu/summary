2ad90fbcc71a93d563f3c3179ea7ccb90a92dcc1
Winn <1147565914@qq.com>
2020-11-24 17:03:41 +0800

    Export Table

--- comm/src/main/java/tools/ExportTable.java
+++ comm/src/main/java/tools/ExportTable.java
@@ -1,13 +1,11 @@
 package tools;
 
-import utils.pojo.DbName;
-
 public class ExportTable {
 
 	public static void main(String[] args) {
 		//导出数据库中的所有表
 		String tableName = "USER_TAB_COMMENTS";
-		ExportTableTools.ExportSingleTable(DbName.ORACLE,tableName);
+		ExportTableTools.ExportSingleTable(tableName);
 	}
 
 }
