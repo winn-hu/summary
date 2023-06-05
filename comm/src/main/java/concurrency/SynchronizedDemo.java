31422be19c387059173e70566c431b1d99fbb885
Winn <1147565914@qq.com>
2023-06-05 10:03:16 +0800

    Synchronized

--- comm/src/main/java/concurrency/SynchronizedDemo.java
+++ comm/src/main/java/concurrency/SynchronizedDemo.java
@@ -7,14 +7,36 @@ package concurrency;
  */
 public class SynchronizedDemo {
 
-    public static synchronized void test() {
+    public  void test(String name) {
+        synchronized(SynchronizedDemo.class) {
+            System.out.println(name + " Synchronized Demo");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
 
+            System.out.println(name + " end.");
+        }
     }
 
     public static void main(String[] args) {
-        synchronized (SynchronizedDemo.class) {
-            System.out.println("Synchronized Demo");
-        }
-       test();
+        SynchronizedDemo demo1 = new SynchronizedDemo();
+        SynchronizedDemo demo2 = new SynchronizedDemo();
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                demo1.test("thread1");
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                demo2.test("thrread2");
+            }
+        });
+
+        thread1.start();
+        thread2.start();
     }
 }
